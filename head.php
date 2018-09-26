0cd2a879a8e6af9cd9af7fc3414a4a9ae7fafd1e
01lijin <3282579826@qq.com>
2018-09-26 15:03:31 +0800

    添加课程列表模块

---
+++ head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>网络投票后台</title>
+	<link rel="stylesheet" type="text/css" href="css/sui.min.css">
+	<link rel="stylesheet" type="text/css" href="css/sui-append.min.css">
+	<link rel="stylesheet" type="text/css" href="css/default.css">
+</head>
+<body>
+<div class="sui-container">
+	<div class="my-head">
+		网络投票后台
+		<a href="index.html" class="userinfo">返回首页</a>
+	</div>
+	
